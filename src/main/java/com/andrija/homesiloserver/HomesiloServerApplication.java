c5d0d9dacd9b1b4a54bec0eed8b3b2900ca5f168
OAndrija <andrija.obradovic@student.um.si>
2026-06-09 21:05:36 +0200

    init commit

---
+++ src/main/java/com/andrija/homesiloserver/HomesiloServerApplication.java
@@ -0,0 +1,13 @@
+package com.andrija.homesiloserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomesiloServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HomesiloServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/andrija/homesiloserver/HomesiloServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.andrija.homesiloserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomesiloServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
