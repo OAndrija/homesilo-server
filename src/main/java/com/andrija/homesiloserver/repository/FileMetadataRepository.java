edd20ccf0f543073685d43b5527e8f40ef2ccc13
OAndrija <andrija.obradovic@student.um.si>
2026-06-16 00:57:55 +0200

    create repository for look up with owner id, trashed boolean and stored filename

---
+++ src/main/java/com/andrija/homesiloserver/repository/FileMetadataRepository.java
@@ -0,0 +1,15 @@
+package com.andrija.homesiloserver.repository;
+
+import com.andrija.homesiloserver.entity.FileMetadata;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+public interface FileMetadataRepository extends JpaRepository<FileMetadata, UUID> {
+    Page<FileMetadata> findByOwnerIdAndTrashedFalse(UUID ownerId, Pageable pageable);
+    Page<FileMetadata> findByOwnerIdAndTrashedTrue(UUID ownerId, Pageable pageable);
+    Optional<FileMetadata> findByStoredFileNameAndOwnerId(String storedFileName, UUID userId);
+}
