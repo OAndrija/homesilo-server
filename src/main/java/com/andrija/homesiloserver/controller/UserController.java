cbe892f9e5b44dac820c4802b9ce3188a1b767ef
OAndrija <andrija.obradovic@student.um.si>
2026-06-18 14:46:04 +0200

    use jwt to get current user instead of accessing DB

--- src/main/java/com/andrija/homesiloserver/controller/UserController.java
+++ src/main/java/com/andrija/homesiloserver/controller/UserController.java
@@ -1,11 +1,11 @@
 package com.andrija.homesiloserver.controller;
 
 import com.andrija.homesiloserver.dto.UserResponse;
+import com.andrija.homesiloserver.security.ServerUserDetails;
 import com.andrija.homesiloserver.service.UserService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.Authentication;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -19,21 +19,22 @@ public class UserController {
     private final UserService userService;
 
     @GetMapping("/me")
-    public ResponseEntity<UserResponse> getCurrentUser(Authentication authentication) {
-        return ResponseEntity.ok(userService.findUserByUsername(authentication.getName()));
+    public ResponseEntity<UserResponse> getCurrentUser(@AuthenticationPrincipal ServerUserDetails userDetails) {
+        UserResponse userResponse = userService.findUserById(userDetails.getId());
+        return ResponseEntity.ok(userResponse);
     }
 
     // Returns a list of all users
     @GetMapping
     public ResponseEntity<List<UserResponse>> listAllUsers() {
-        List<UserResponse> users = userService.listAllUsers();
-        return ResponseEntity.status(HttpStatus.OK).body(users);
+        List<UserResponse> usersResponse = userService.listAllUsers();
+        return ResponseEntity.ok(usersResponse);
     }
 
     @GetMapping("/{id}")
     public ResponseEntity<UserResponse> getUserById(@PathVariable UUID id) {
-        UserResponse user = userService.findUserById(id);
-        return ResponseEntity.status(HttpStatus.OK).body(user);
+        UserResponse userResponse = userService.findUserById(id);
+        return ResponseEntity.ok(userResponse);
     }
 
     @GetMapping("/search")
